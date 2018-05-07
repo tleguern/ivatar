fe3f198b4449a8d6dae81832671a783f1762a0e8
Oliver Falk <oliver@linux-kernel.at>
2018-05-07 15:34:27 +0200

    User creation + password change tests

--- ivatar/ivataraccount/test_views.py
+++ ivatar/ivataraccount/test_views.py
@@ -21,9 +21,31 @@ class Tester(TestCase):
         response = self.client.get(reverse('new_account'))
         self.assertEqual(response.status_code, 200, 'no 200 ok?')
         url = reverse('new_account')
-        response = self.client.post(url, {
-            'username': self.username,
-            'password1': self.password,
-            'password2': self.password,
-        })
-        print(response)
+        response = self.client.post(
+            url, {
+                'username': self.username,
+                'password1': self.password,
+                'password2': self.password,
+            },
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200, 'unable to create user?')
+        self.assertEqual(response.context[0]['user'].username, self.username)
+
+    def test_set_password(self):
+        """
+        Change the user password
+        """
+        response = self.client.get(reverse('password_set'))
+        self.assertEqual(response.status_code, 200, 'no 200 ok?')
+        url = reverse('password_set')
+        self.password = random_string()
+        response = self.client.post(
+            url, {
+                'password1': self.password,
+                'password2': self.password,
+            },
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200, 'cannot change password?')
+        self.assertEqual(response.context[0]['user'].is_anonymous, True)
