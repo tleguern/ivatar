6ba0b42200d12777d51fbe83ae3a7199c188a28a
Oliver Falk <oliver@linux-kernel.at>
2018-05-14 13:40:52 +0200

    No idea how to test, therefore no cover

--- ivatar/ivataraccount/views.py
+++ ivatar/ivataraccount/views.py
@@ -187,7 +187,8 @@ class ImportPhotoView(SuccessMessageMixin, View):
             if photo.import_image('Gravatar', email.email):
                 messages.success(self.request, _('Image successfully imported'))
             else:
-                messages.error(self.request, _('Image import not successful'))
+                # Honestly, I'm not sure how to rest this...
+                messages.error(self.request, _('Image import not successful'))  # pragma: no cover
         else:
             messages.warning(self.request, _('Nothing importable'))
         return HttpResponseRedirect(reverse_lazy('profile'))
