63f40d6416116cf9d53d6c47c899225baaeb9bbf
Oliver Falk <oliver@linux-kernel.at>
2018-05-14 12:34:00 +0200

    This code can never happen, since if the email is already confirmed, the verification key does not longer exist and therefore the function will exit, before this code can be reached

--- ivatar/ivataraccount/views.py
+++ ivatar/ivataraccount/views.py
@@ -105,12 +105,7 @@ class ConfirmEmailView(SuccessMessageMixin, TemplateView):
             messages.error(self.request, _('Verification key does not exist'))
             return HttpResponseRedirect(reverse_lazy('profile'))
 
-        # TODO: Check for a reansonable expiration time in unconfirmed email
-
-        # check to see whether this email is already confirmed
-        if ConfirmedEmail.objects.filter(email=unconfirmed.email).exists():
-            messages.warning(self.request, _('Address already confirmed'))
-            return HttpResponseRedirect(reverse_lazy('profile'))
+        # TODO: Check for a reasonable expiration time in unconfirmed email
 
         (confirmed_id, external_photos) = ConfirmedEmail.objects.create_confirmed_email(
             unconfirmed.user, unconfirmed.email, not self.request.user.is_anonymous)
