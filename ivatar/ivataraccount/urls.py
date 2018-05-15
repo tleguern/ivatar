652350e0332384dc2cf47bc383833871aeb01f0c
Oliver Falk <oliver@linux-kernel.at>
2018-05-15 13:20:02 +0200

    Wire up OpenID actions

--- ivatar/ivataraccount/urls.py
+++ ivatar/ivataraccount/urls.py
@@ -5,7 +5,9 @@ from django.views.generic import TemplateView
 from . views import CreateView, PasswordSetView, AddEmailView
 from . views import RemoveUnconfirmedEmailView, ConfirmEmailView
 from . views import RemoveConfirmedEmailView, AssignPhotoEmailView
+from . views import RemoveUnconfirmedOpenIDView, RemoveConfirmedOpenIDView
 from . views import ImportPhotoView, RawImageView, DeletePhotoView, UploadPhotoView
+from . views import AddOpenIDView, RedirectOpenIDView, ConfirmOpenIDView
 from django.contrib.auth.views import login, logout, password_change, password_change_done
 from django.urls import reverse_lazy
 
@@ -28,15 +30,18 @@ urlpatterns = [
         TemplateView.as_view(template_name='profile.html')
     ), name='profile'),
     path('add_email/', AddEmailView.as_view(), name='add_email'),
-    path('add_openid/', login_required(
-        TemplateView.as_view(template_name='add_openid.html')
-    ), name='add_openid'),
+    path('add_openid/', AddOpenIDView.as_view(), name='add_openid'),
     path('upload_photo/', UploadPhotoView.as_view(), name='upload_photo'),
     path('password_set/', PasswordSetView.as_view(), name='password_set'),
+    url('remove_unconfirmed_openid/(?P<openid_id>\d+)', RemoveUnconfirmedOpenIDView.as_view(), name='remove_unconfirmed_openid'),
+    url('remove_confirmed_openid/(?P<openid_id>\d+)', RemoveConfirmedOpenIDView.as_view(), name='remove_confirmed_openid'),
+    url('openid_redirection/(?P<openid_id>\d+)', RedirectOpenIDView.as_view(), name='openid_redirection'),
+    url('confirm_openid/(?P<openid_id>\w+)', ConfirmOpenIDView.as_view(), name='confirm_openid'),
     url('confirm_email/(?P<verification_key>\w+)', ConfirmEmailView.as_view(), name='confirm_email'),
     url('remove_unconfirmed_email/(?P<email_id>\d+)', RemoveUnconfirmedEmailView.as_view(), name='remove_unconfirmed_email'),
     url('remove_confirmed_email/(?P<email_id>\d+)', RemoveConfirmedEmailView.as_view(), name='remove_confirmed_email'),
     url('assign_photo_email/(?P<email_id>\d+)', AssignPhotoEmailView.as_view(), name='assign_photo_email'),
+    url('assign_photo_openid/(?P<email_id>\d+)', TemplateView.as_view(template_name='fixme.html'), name='assign_photo_openid'),
     url('import_photo/(?P<email_id>\d+)', ImportPhotoView.as_view(), name='import_photo'),
     url('delete_photo/(?P<pk>\d+)', DeletePhotoView.as_view(), name='delete_photo'),
     url('raw_image/(?P<pk>\d+)', RawImageView.as_view(), name='raw_image'),
