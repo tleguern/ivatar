088be58723457982560dc18dc830faf259399223
Oliver Falk <oliver@linux-kernel.at>
2018-05-23 10:36:14 +0200

    PEP8

--- ivatar/ivataraccount/urls.py
+++ ivatar/ivataraccount/urls.py
@@ -9,7 +9,7 @@ from . views import RemoveUnconfirmedOpenIDView, RemoveConfirmedOpenIDView
 from . views import ImportPhotoView, RawImageView, DeletePhotoView
 from . views import UploadPhotoView, AssignPhotoOpenIDView
 from . views import AddOpenIDView, RedirectOpenIDView, ConfirmOpenIDView
-from django.contrib.auth.views import login, logout, password_change, password_change_done
+from django.contrib.auth.views import login, logout
 from django.urls import reverse_lazy
 
 from django.contrib.auth.decorators import login_required
@@ -19,8 +19,10 @@ from django.contrib.auth.decorators import login_required
 # ./manager show_urls
 urlpatterns = [
     path('new/', CreateView.as_view(), name='new_account'),
-    path('login/', login, { 'template_name': 'login.html' }, name='login'),
-    path('logout/', logout, { 'next_page': reverse_lazy('login') }, name='logout'),
+    path('login/', login, {'template_name': 'login.html'}, name='login'),
+    path(
+        'logout/', logout, {'next_page': reverse_lazy('login')},
+        name='logout'),
     path('export/', login_required(
         TemplateView.as_view(template_name='export.html')
     ), name='export'),
@@ -34,16 +36,39 @@ urlpatterns = [
     path('add_openid/', AddOpenIDView.as_view(), name='add_openid'),
     path('upload_photo/', UploadPhotoView.as_view(), name='upload_photo'),
     path('password_set/', PasswordSetView.as_view(), name='password_set'),
-    url('remove_unconfirmed_openid/(?P<openid_id>\d+)', RemoveUnconfirmedOpenIDView.as_view(), name='remove_unconfirmed_openid'),
-    url('remove_confirmed_openid/(?P<openid_id>\d+)', RemoveConfirmedOpenIDView.as_view(), name='remove_confirmed_openid'),
-    url('openid_redirection/(?P<openid_id>\d+)', RedirectOpenIDView.as_view(), name='openid_redirection'),
-    url('confirm_openid/(?P<openid_id>\w+)', ConfirmOpenIDView.as_view(), name='confirm_openid'),
-    url('confirm_email/(?P<verification_key>\w+)', ConfirmEmailView.as_view(), name='confirm_email'),
-    url('remove_unconfirmed_email/(?P<email_id>\d+)', RemoveUnconfirmedEmailView.as_view(), name='remove_unconfirmed_email'),
-    url('remove_confirmed_email/(?P<email_id>\d+)', RemoveConfirmedEmailView.as_view(), name='remove_confirmed_email'),
-    url('assign_photo_email/(?P<email_id>\d+)', AssignPhotoEmailView.as_view(), name='assign_photo_email'),
-    url('assign_photo_openid/(?P<openid_id>\d+)', AssignPhotoOpenIDView.as_view(), name='assign_photo_openid'),
-    url('import_photo/(?P<email_id>\d+)', ImportPhotoView.as_view(), name='import_photo'),
-    url('delete_photo/(?P<pk>\d+)', DeletePhotoView.as_view(), name='delete_photo'),
+    url(
+        'remove_unconfirmed_openid/(?P<openid_id>\d+)',
+        RemoveUnconfirmedOpenIDView.as_view(),
+        name='remove_unconfirmed_openid'),
+    url(
+        'remove_confirmed_openid/(?P<openid_id>\d+)',
+        RemoveConfirmedOpenIDView.as_view(), name='remove_confirmed_openid'),
+    url(
+        'openid_redirection/(?P<openid_id>\d+)',
+        RedirectOpenIDView.as_view(), name='openid_redirection'),
+    url(
+        'confirm_openid/(?P<openid_id>\w+)',
+        ConfirmOpenIDView.as_view(), name='confirm_openid'),
+    url(
+        'confirm_email/(?P<verification_key>\w+)',
+        ConfirmEmailView.as_view(), name='confirm_email'),
+    url(
+        'remove_unconfirmed_email/(?P<email_id>\d+)',
+        RemoveUnconfirmedEmailView.as_view(), name='remove_unconfirmed_email'),
+    url(
+        'remove_confirmed_email/(?P<email_id>\d+)',
+        RemoveConfirmedEmailView.as_view(), name='remove_confirmed_email'),
+    url(
+        'assign_photo_email/(?P<email_id>\d+)',
+        AssignPhotoEmailView.as_view(), name='assign_photo_email'),
+    url(
+        'assign_photo_openid/(?P<openid_id>\d+)',
+        AssignPhotoOpenIDView.as_view(), name='assign_photo_openid'),
+    url(
+        'import_photo/(?P<email_id>\d+)',
+        ImportPhotoView.as_view(), name='import_photo'),
+    url(
+        'delete_photo/(?P<pk>\d+)',
+        DeletePhotoView.as_view(), name='delete_photo'),
     url('raw_image/(?P<pk>\d+)', RawImageView.as_view(), name='raw_image'),
 ]
