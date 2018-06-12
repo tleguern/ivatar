c0dc55d039fa13a8110d4d221cff63cd7645d957
Oliver Falk <oliver@linux-kernel.at>
2018-06-12 14:51:34 +0200

    Add example deployment script

---
+++ create.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+oc new-project ivatar
+# Take care, the following environment variable must be set either in bashrc/profile or locally in this session
+oc secrets new-basicauth lkernat-gitlab-openshift-falko-access-token --password=$LKERNAT_GITLAB_OPENSHIFT_ACCESS_TOKEN
+oc secrets add serviceaccount/builder secrets/lkernat-gitlab-openshift-falko-access-token
+MYSQL_PASSWORD=`openssl rand -base64 16`
+MYSQL_ROOT_PASSWORD=`openssl rand -base64 16`
+oc new-app --source-secret=lkernat-gitlab-openshift-falko-access-token \
+  python~https://git.linux-kernel.at/oliver/ivatar.git \
+  mysql-persistent \
+  --group=python+mysql-persistent \
+  -e MYSQL_USER=ivatar \
+  -p MYSQL_USER=ivatar \
+  -e MYSQL_PASSWORD=$MYSQL_PASSWORD \
+  -p MYSQL_PASSWORD=$MYSQL_PASSWORD \
+  -e MYSQL_DATABASE=ivatar \
+  -p MYSQL_DATABASE=ivatar \
+  -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
+  -p MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
+oc expose svc/ivatar
