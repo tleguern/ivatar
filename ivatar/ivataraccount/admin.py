1c355b50146620982f372b23d7a7c8b9590a7446
Oliver Falk <oliver@linux-kernel.at>
2018-07-09 13:29:20 +0200

    Add additional models in admin

--- ivatar/ivataraccount/admin.py
+++ ivatar/ivataraccount/admin.py
@@ -4,12 +4,16 @@ Register models in admin
 from django.contrib import admin
 
 from . models import Photo, ConfirmedEmail, UnconfirmedEmail
-from . models import ConfirmedOpenId, OpenIDNonce, OpenIDAssociation
+from . models import ConfirmedOpenId, UnconfirmedOpenId
+from . models import OpenIDNonce, OpenIDAssociation
+from . models import UserPreference
 
 # Register models in admin
 admin.site.register(Photo)
 admin.site.register(ConfirmedEmail)
 admin.site.register(UnconfirmedEmail)
 admin.site.register(ConfirmedOpenId)
+admin.site.register(UnconfirmedOpenId)
+admin.site.register(UserPreference)
 admin.site.register(OpenIDNonce)
 admin.site.register(OpenIDAssociation)
