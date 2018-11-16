tleguern/ivatar

commit c3ed5fb014bf268d9d8f9ca12e4adebd2faf1130

Fix issue #17

ivatar/views.py
@@ -54,7 +54,12 @@ class AvatarImageView(TemplateView):
             sizetemp = request.GET['size']
         if sizetemp:
             if sizetemp != '' and sizetemp is not None and sizetemp != '0':
-                size = int(sizetemp)
+                try:
+                    size = int(sizetemp)
+                # Should we receive something we cannot convert to int, leave
+                # the user with the default value of 80
+                except ValueError:
+                    pass
 
         if size > int(AVATAR_MAX_SIZE):
             size = int(AVATAR_MAX_SIZE)

commit c6710052d2a4d9514e78f6b480108225f14224bb

Fix regression introduced by commit c3ed5fb0 (issue #18 - fixes issue #20

ivatar/views.py
@@ -127,7 +127,12 @@ class AvatarImageView(TemplateView):
 
                 if str(default) == 'mm' or str(default) == 'mp':
                     # If mm is explicitly given, we need to catch that
-                    pass
+                    static_img = path.join('static', 'img', 'mm', '%s%s' % (str(size), '.png'))
+                    if not path.isfile(static_img):
+                        # We trust this exists!!!
+                        static_img = path.join('static', 'img', 'mm', '512.png')
+                    # We trust static/ is mapped to /static/
+                    return HttpResponseRedirect('/' + static_img)
                 else:
                     return HttpResponseRedirect(default)
 